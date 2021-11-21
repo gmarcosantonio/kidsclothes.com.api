9b46c516a046709150c7553fb8dddd57f74d3801
gmarcosantonio <63070598+gmarcosantonio@users.noreply.github.com>
2021-11-21 07:26:02 -0300

    make it better

---
+++ src/main/java/kidsclothes/com/api/controller/WelcomeController.java
@@ -0,0 +1,16 @@
+package kidsclothes.com.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class WelcomeController {
+
+	@GetMapping(value = "/wecome")
+	public String welcome()  {
+
+		return "Welcome to API Kidsclothes.COM";
+	}
+
+}
